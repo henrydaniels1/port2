fd2a4be1cbba381dfd1762fd5d7b1ccff41425fc
henrydaniels1 <henrydanielchibuzor@gmail.com>
2024-09-11 21:01:17 +0100

    new14

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import './Loader.css'; // Ensure this path is correct
 
 export default function Loader() {
     return ( 
-      <div className=" bg-gray-900 flex justify-center items-center h-screen">
+      <div className="main-loader bg-gray-900 flex justify-center items-center">
   <div className="loader "></div>
 </div>
   );
